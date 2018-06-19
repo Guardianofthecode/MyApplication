716b6f513f506cc4520e24ba8065ceca4f32e32e
KotrotsKon <kotrotskon@users.noreply.github.com>
2018-06-19 19:44:26 +0300

    add two numbers

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,23 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText edTxt_read = findViewById(R.id.edTxt_text1);
-        final EditText edTxt_write = findViewById(R.id.edTxt_text2);
+        final EditText edTxt_num1 = findViewById(R.id.edTxt_num1);
+        final EditText edTxt_num2 = findViewById(R.id.edTxt_num2);
+        final EditText edTxt_result = findViewById(R.id.edTxt_result);
 
-        Button btn = findViewById(R.id.btn);
+        Button btn_add = findViewById(R.id.btn_add);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String myText = edTxt_read.getText().toString();
-                edTxt_write.setText(myText);
+
+                String str_num1 = edTxt_num1.getText().toString();
+                String str_num2 = edTxt_num2.getText().toString();
+
+                double num1 = Double.valueOf(str_num1);
+                double num2 = Double.valueOf(str_num2);
+
+                double result = num1 + num2;
+
+                String str_result = String.valueOf(result);
+
+                edTxt_result.setText(str_result);
             }
         });
-
-
-
-
-
-
     }
 }
