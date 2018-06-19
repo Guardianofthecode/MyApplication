bbd7ee559e01906b83ec8ba8b5a1cbe329f42e68
KotrotsKon <kotrotskon@users.noreply.github.com>
2018-06-19 19:06:59 +0300

    read text and show in editText

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edTxt = findViewById(R.id.edTxt_text1);
+        final EditText edTxt_read = findViewById(R.id.edTxt_text1);
+        final EditText edTxt_write = findViewById(R.id.edTxt_text2);
 
-        edTxt.setText("this is some text");
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String myText = edTxt_read.getText().toString();
+                edTxt_write.setText(myText);
+            }
+        });
+
 
 
 
