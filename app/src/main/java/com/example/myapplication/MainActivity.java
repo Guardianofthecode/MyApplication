d83cf42b546b180207a08b45d0f9ce6e0d5cbf5c
GUARDIANOFTHECODE <reg@futurenet.gr>
2018-06-20 21:10:50 +0300

    Exercise 1 - Add buttons for ADD SUB MULTI DIV

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         final EditText edTxt_result = findViewById(R.id.edTxt_result);
 
         Button btn_add = findViewById(R.id.btn_add);
+        Button btn_sub = findViewById(R.id.btn_sub);
+        Button btn_multi = findViewById(R.id.btn_multi);
+        Button btn_div = findViewById(R.id.btn_div);
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +39,61 @@ public class MainActivity extends AppCompatActivity {
                 edTxt_result.setText(str_result);
             }
         });
+
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String str_num1 = edTxt_num1.getText().toString();
+                String str_num2 = edTxt_num2.getText().toString();
+
+                double num1 = Double.valueOf(str_num1);
+                double num2 = Double.valueOf(str_num2);
+
+                double result = num1 - num2;
+
+                String str_result = String.valueOf(result);
+
+                edTxt_result.setText(str_result);
+            }
+        });
+
+
+        btn_multi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String str_num1 = edTxt_num1.getText().toString();
+                String str_num2 = edTxt_num2.getText().toString();
+
+                double num1 = Double.valueOf(str_num1);
+                double num2 = Double.valueOf(str_num2);
+
+                double result = num1 * num2;
+
+                String str_result = String.valueOf(result);
+
+                edTxt_result.setText(str_result);
+            }
+        });
+
+        btn_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String str_num1 = edTxt_num1.getText().toString();
+                String str_num2 = edTxt_num2.getText().toString();
+
+                double num1 = Double.valueOf(str_num1);
+                double num2 = Double.valueOf(str_num2);
+
+                double result = num1 / num2;
+
+                String str_result = String.valueOf(result);
+
+                edTxt_result.setText(str_result);
+            }
+        });
+
     }
 }
